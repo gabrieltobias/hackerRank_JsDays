b45b70cbefdf6dfeef725ec50facbd16cba7f896
Gabriel Tobias <gabrielvieiratobias@gmail.com>
2019-08-16 19:46:47 -0300

    1608

---
+++ letConst.js
@@ -0,0 +1,48 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    const PI = Math.PI;
+    let r = parseFloat(readLine());
+    let area = PI * (r * r);
+    let per = (2 * PI) * r;
+    // Print the area of the circle:
+    console.log(area);
+    // Print the perimeter of the circle:
+     console.log(per);
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+
+    // Print the area of the circle:
+
+    // Print the perimeter of the circle:
+
+    try {
+        // Attempt to redefine the value of constant variable PI
+        PI = 0;
+        // Attempt to print the value of PI
+        console.log(PI);
+    } catch(error) {
+        console.error("You correctly declared 'PI' as a constant.");
+    }
+}
